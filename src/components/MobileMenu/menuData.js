0373fbdf4c588c8ce532bad66d6a7b8e30aa88b1
Georgio Charro <georgiocharro@hotmail.com>
2024-11-03 22:29:50 +0200

    [Web] Removed unacessory categories

--- src/components/MobileMenu/menuData.js
+++ src/components/MobileMenu/menuData.js
@@ -1,79 +1,70 @@
-import React from 'react';
-import { CgChevronDown, CgChevronLeft } from 'react-icons/cg';
+import React from "react";
+import { CgChevronDown, CgChevronLeft } from "react-icons/cg";
 
 const menuData = [
-    {
-        title: 'Home',
-        path: '/',
-        iconClosed: <CgChevronLeft />,
-        iconOpened: <CgChevronDown />,
+  {
+    title: "Home",
+    path: "/",
+    iconClosed: <CgChevronLeft />,
+    iconOpened: <CgChevronDown />,
+  },
 
-    },
+  {
+    title: "About",
+    path: "/about",
+  },
 
-    {
-        title: 'About',
-        path: '/about',
-    },
+  {
+    title: "Category",
+    path: "/",
 
-    {
-        title: 'Category',
-        path: '/',
+    iconClosed: <CgChevronLeft />,
+    iconOpened: <CgChevronDown />,
 
-        iconClosed: <CgChevronLeft />,
-        iconOpened: <CgChevronDown />,
+    subNav: [
+      {
+        title: "Thyme And Spices",
+        path: "/category/Thyme%20and%20Spices",
+        cName: "sub-nav",
+      },
+      {
+        title: "Nuts",
+        path: "/category/nuts",
+        cName: "sub-nav",
+      },
+      {
+        title: "Chocolate",
+        path: "/category/Chocolate",
+      },
+      {
+        title: "Dairy",
+        path: "/category/dairy",
+      },
+      {
+        title: "Filling Machines",
+        path: "/category/Filling",
+      },
+      {
+        title: "Filtering",
+        path: "/category/Filtering",
+      },
 
-        subNav: [
-            {
-                title: 'Thyme And Spices',
-                path: '/category/Thyme%20and%20Spices',
-                cName: 'sub-nav',
-            },
-            {
-                title: 'Nuts',
-                path: '/category/nuts',
-                cName: 'sub-nav',
-            },
-            {
-                title: 'Chocolate',
-                path: '/category/Chocolate',
-            },
-            {
-                title: 'Dairy',
-                path: '/category/dairy',
-            },
-            {
-                title: 'Filling Machines',
-                path: '/category/Filling',
-            },
-            {
-                title: 'Filtering',
-                path: '/category/Filtering',
-            },
-            {
-                title: 'Stainless',
-                path: '/category/Stainless',
-            },
-            {
-                title: 'Tahina',
-                path: '/category/Tahina',
-            },
-            {
-                title: 'Others',
-                path: '/category/Others',
-            }
-        ],
-    },
+      {
+        title: "Tahina",
+        path: "/category/Tahina",
+      },
+    ],
+  },
 
-    {
-        title: 'Services',
-        path: '/services',
-    },
+  {
+    title: "Services",
+    path: "/services",
+  },
 
-
-    {
-        title: 'Contact',
-        path: '/contact',
-    },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
 ];
 
 export default menuData;
